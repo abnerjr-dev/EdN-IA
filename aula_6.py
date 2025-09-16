dfb56ed05f700dccf7c3523ac3652f42208c2cee
abnerjr-dev <141677050+abnerjr-dev@users.noreply.github.com>
2025-09-16 19:41:00 -0300

    Add files via upload

---
+++ aula_6.py
@@ -0,0 +1,26 @@
+print("1- Programa de Saudação")
+print("Olá, mundo!")
+
+print("2- Calculadora de Soma")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print(f"A soma de {num1} e {num2} é {num1 + num2}.")
+
+print("3- Calculadora de Volume")
+c = 12  # float(input("Digite o comprimento: "))
+l = 14  # float(input("Digite a largura: "))
+a = 20  # float(input("Digite a altura: "))
+Volume = c * l * a
+print("O volume é:", Volume, "cm³")
+
+print("4- Calculadora de Preço Total")
+
+nome_produto = "Cadeira Infantil"  # input("Digite o nome do produto: ")
+preco_unitario = 12.40  # float(input("Digite o preço unitário: "))
+quantidade = 3  # int(input("Digite a quantidade: "))
+preco_total = preco_unitario * quantidade
+print(
+    f"Você comprou {quantidade} unidades de {nome_produto}, O preço total é: R$ {preco_total:.2f}"
+)
